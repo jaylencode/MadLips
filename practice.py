9a9619f64d1648517e75e30675a0553af2543209
Jahod <jcode1699@gmail.com>
2022-08-17 17:43:35 -0400

    first commit

---
+++ practice.py
@@ -0,0 +1,12 @@
+
+
+# print("Subscribe to " + youtuber)
+# print("Subscribe to {}".format("youtuber"))
+# print("Subscribe to {youtuber}")
+abj = input("Adjective: ")
+verb1 = input("Verb1: ")
+verb2 = input("Verb1: ")
+famous_person = input("Famous person: ")
+mablib = f"Computer programming is so {abj}! It make me so excited all the time because \ I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(mablib)
